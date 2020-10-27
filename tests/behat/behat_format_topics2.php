7302fb6f762858f4f3ac8d8025c564decf774741
Matthias Opitz <opitz@gmx.de>
2020-10-27 11:31:25 +0000

    behat 201027-009

--- tests/behat/behat_format_topics2.php
+++ tests/behat/behat_format_topics2.php
@@ -26,7 +26,7 @@
 // NOTE: no MOODLE_INTERNAL test here, this file may be required by behat before including /config.php.
 
 require_once(__DIR__ . '/../../../../../lib/behat/behat_base.php');
-require_once(__DIR__ . '/../../../../../../course/tests/behat/behat_course.php');
+require_once(__DIR__ . '/../../../../../course/tests/behat/behat_course.php');
 
 /**
  * Steps definitions related with putting sections under tabs.
